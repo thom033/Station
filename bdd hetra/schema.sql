6a5edee3f46f0bc99131074672a2327a4370f7ca
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-21 10:08:50 +0300

    Update schema.sql
    update id integer to id varchar

--- bdd hetra/schema.sql
+++ bdd hetra/schema.sql
@@ -20,21 +20,21 @@ CREATE TABLE maison (
 
 CREATE TABLE maison_detaills (
     id_maison_detaills NUMBER PRIMARY KEY,
-    id_maison NUMBER,
-    id_type_rindrina NUMBER,
-    id_type_tafo NUMBER,
+    id_maison varchar(50),
+    id_type_rindrina varchar(50),
+    id_type_tafo varchar(50),
     FOREIGN KEY (id_maison) REFERENCES maison(id),
     FOREIGN KEY (id_type_rindrina) REFERENCES type_rindrina(id_type_rindrina),
     FOREIGN KEY (id_type_tafo) REFERENCES type_tafo(id_type_tafo)
 );
 
 CREATE TABLE arrondissement (
-    id_arrondissement NUMBER PRIMARY KEY,
+    id_arrondissement varchar(50) PRIMARY KEY,
     nom VARCHAR2(100)
 );
 
 CREATE TABLE arrondissement_position (
-    id_arrondissement NUMBER,
+    id_arrondissement varchar(50),
     position SDO_GEOMETRY,
     FOREIGN KEY (id_arrondissement) REFERENCES arrondissement(id_arrondissement)
 );
