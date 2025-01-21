c82b24106bf7cda1cb8f58f368443accb3071038
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-21 10:29:40 +0300

    Update sequence.sql
    add functions to get sequence value

--- bdd hetra/sequence.sql
+++ bdd hetra/sequence.sql
@@ -53,3 +53,59 @@ START WITH 1
 INCREMENT BY 1
 NOCACHE
 NOCYCLE;
+
+-- Function for seq_type_tafo
+CREATE OR REPLACE FUNCTION get_seq_type_tafo RETURN NUMBER IS
+BEGIN
+    RETURN seq_type_tafo.NEXTVAL;
+END;
+/
+
+-- Function for seq_type_rindrina
+CREATE OR REPLACE FUNCTION get_seq_type_rindrina RETURN NUMBER IS
+BEGIN
+    RETURN seq_type_rindrina.NEXTVAL;
+END;
+/
+
+-- Function for seq_maison
+CREATE OR REPLACE FUNCTION get_seq_maison RETURN NUMBER IS
+BEGIN
+    RETURN seq_maison.NEXTVAL;
+END;
+/
+
+-- Function for seq_maison_detaills
+CREATE OR REPLACE FUNCTION get_seq_maison_detaills RETURN NUMBER IS
+BEGIN
+    RETURN seq_maison_detaills.NEXTVAL;
+END;
+/
+
+-- Function for seq_arrondissement
+CREATE OR REPLACE FUNCTION get_seq_arrondissement RETURN NUMBER IS
+BEGIN
+    RETURN seq_arrondissement.NEXTVAL;
+END;
+/
+
+-- Function for seq_arrondissement_position
+CREATE OR REPLACE FUNCTION get_seq_arrondissement_p RETURN NUMBER IS
+BEGIN
+    RETURN seq_arrondissement_position.NEXTVAL;
+END;
+/
+
+-- Function for seq_hetra
+CREATE OR REPLACE FUNCTION get_seq_hetra RETURN NUMBER IS
+BEGIN
+    RETURN seq_hetra.NEXTVAL;
+END;
+/
+
+-- Function for seq_paiementP
+CREATE OR REPLACE FUNCTION get_seq_paiementP RETURN NUMBER IS
+BEGIN
+    RETURN seq_paiementP.NEXTVAL;
+END;
+/
