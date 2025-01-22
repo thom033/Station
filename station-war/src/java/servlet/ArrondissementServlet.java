ed3dbe8d57053f26a0a2e15690b7d2d35c57d16c
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-22 15:32:39 +0300

    Create ArrondissementServlet.java

---
+++ station-war/src/java/servlet/ArrondissementServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.CGenUtil;
+import hetra.carte.Arrondissement;
+
+@WebServlet("/arrondissement")
+public class ArrondissementServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            request.setAttribute("list", CGenUtil.rechercher(new Arrondissement(), "select * from arrondissement"));
+            request.getRequestDispatcher("/hetra/arrondissement-list").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
