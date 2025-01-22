6a7b8ff7af886947d2c8c3ea15cfc58267a7d206
RASOLOMANDIMBY Nomenjanahary Thomis <Thomis.rasolomandimby@gmail.com>
2025-01-22 10:39:45 +0300

    get typetafo && typeRindrina Servlet Doget

--- station-war/src/java/servlet/CarteServlet.java
+++ station-war/src/java/servlet/CarteServlet.java
@@ -9,13 +9,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.CGenUtil;
+
+import hetra.trano.TypeRindrina;
+import hetra.trano.TypeTafo;
+
 @WebServlet( urlPatterns = {"/carte"})
 public class CarteServlet  extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
+            TypeRindrina typeRindrina = new TypeRindrina();
+            TypeTafo typeTafo = new TypeTafo();
 
+            TypeRindrina[] typeRindrinaList = (TypeRindrina[]) CGenUtil.rechercher(typeRindrina, null, null, null, "");
+            TypeTafo[] typeTafoList = (TypeTafo[]) CGenUtil.rechercher(typeTafo, null, null, null, "");
+            req.setAttribute("typeRindrinaList", typeRindrinaList);
+            req.setAttribute("typeTafoList", typeTafoList);
             
         } catch (Exception e) {
             e.printStackTrace();
