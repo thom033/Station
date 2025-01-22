fc488b49a8d30677e69b5513d9492386eb7c1cc9
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-22 13:46:52 +0300

    Create data.sql

---
+++ bdd hetra/data.sql
@@ -0,0 +1,10 @@
+-- Insérer des données de référence dans la table type_tafo
+INSERT INTO type_tafo (id_type_tafo, nom, coefficient) VALUES (1, 'Tafo falafa', 1.0);
+INSERT INTO type_tafo (id_type_tafo, nom, coefficient) VALUES (2, 'Tafo tôle', 1.5);
+INSERT INTO type_tafo (id_type_tafo, nom, coefficient) VALUES (3, 'Tafo ravinala', 1.2);
+
+-- Insérer des données de référence dans la table type_rindrina
+INSERT INTO type_rindrina (id_type_rindrina, nom, coefficient) VALUES (1, 'Rindrina falafa', 1.0);
+INSERT INTO type_rindrina (id_type_rindrina, nom, coefficient) VALUES (2, 'Rindrina brique', 1.5);
+INSERT INTO type_rindrina (id_type_rindrina, nom, coefficient) VALUES (3, 'Rindrina bois', 1.2);
+INSERT INTO type_rindrina (id_type_rindrina, nom, coefficient) VALUES (4, 'Rindrina beton', 2.0);
