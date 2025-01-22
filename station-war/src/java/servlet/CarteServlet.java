13ba0a20a82d27fe02ac7f9a290ab7f43061955c
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-22 13:46:41 +0300

    Update CarteServlet.java

--- station-war/src/java/servlet/CarteServlet.java
+++ station-war/src/java/servlet/CarteServlet.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.sql.Connection;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -23,20 +22,7 @@ public class CarteServlet  extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            Gson gson = new Gson();
-
-            TypeData data = new TypeData();
-
-            resp.setHeader("Access-Control-Allow-Origin", "*");
-            resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-            String jsonResponse = gson.toJson(data);
-            resp.setContentType("application/json");
-            resp.setCharacterEncoding("UTF-8");
-            resp.getWriter().write(jsonResponse);
-            
+        try {            
             req.getRequestDispatcher("/pages/hetra/carte.jsp").forward(req, resp);
         } catch (Exception e) {
             e.printStackTrace();
@@ -46,6 +32,10 @@ public class CarteServlet  extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "PST");
+            response.setHeader("Access-Control-Allow-Headers", "Content-Type, application/json");
+
             // Lire les données JSON depuis la requête
             BufferedReader reader = request.getReader();
             StringBuilder json = new StringBuilder();
@@ -53,6 +43,7 @@ public class CarteServlet  extends HttpServlet{
             while ((line = reader.readLine()) != null) {
                 json.append(line);
             }
+            System.out.println(json.toString());
 
             // Convertir le JSON en objet Java
             ObjectMapper objectMapper = new ObjectMapper();
@@ -60,13 +51,15 @@ public class CarteServlet  extends HttpServlet{
 
             // Récupérer les valeurs
             String nom = (String) data.get("nom");
-            double longueur = (double) data.get("longueur");
-            double largeur = (double) data.get("largeur");
-            int nbrEtage = (int) data.get("nbrEtage");
-            double latitude = (double) data.get("latitude");
-            double longitude = (double) data.get("longitude");
+            double longueur = Double.parseDouble(data.get("longueur").toString());
+            double largeur = Double.parseDouble(data.get("largeur").toString());
+            int nbrEtage = Integer.parseInt( data.get("nbrEtage").toString());
+            double latitude = Double.parseDouble( data.get("latitude").toString());
+            double longitude = Double.parseDouble( data.get("longitude").toString());
 
             Maison m = new Maison(nom, longueur, largeur, nbrEtage, latitude, longitude);
+            MaisonDetails details = new MaisonDetails(data.get("typeTafo").toString(),data.get("typeRindrina").toString());
+            m.setDetails(details);
             m.insertToTable();
 
             response.setStatus(HttpServletResponse.SC_OK);
