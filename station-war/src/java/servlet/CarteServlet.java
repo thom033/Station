6d9ef3a337104efe6a45bdc1fc65d341e501732b
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-22 15:20:09 +0300

    fixed insert Maison

--- station-war/src/java/servlet/CarteServlet.java
+++ station-war/src/java/servlet/CarteServlet.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
-
 import hetra.trano.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
