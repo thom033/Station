8849cca0f4b55a5eb1c23acfb0991f0c019b7f36
RASOLOMANDIMBY Nomenjanahary Thomis <Thomis.rasolomandimby@gmail.com>
2025-01-22 10:27:30 +0300

    Adding class
    Nanampy class TypeRindrina et TypeTafo

---
+++ station-ejb/src/java/hetra/trano/TypeRindrina.java
@@ -0,0 +1,54 @@
+package hetra.trano;
+
+import java.sql.Connection;
+
+import bean.ClassMere;
+
+public class TypeRindrina extends ClassMAPTable{
+    String id,nom;
+    int coefficient;
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getCoefficient() {
+        return coefficient;
+    }
+
+    public void setCoefficient(int coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public TypeRindrina () {
+        this.setNomTable("type_rindrina");
+    }
+
+    @Override
+    public void construirePK(Connection c) throws Exception {
+        this.preparePk("RN", "seq_type_rindrina");
+        this.setId(makePK(c));
+    }
+
+    @Override
+    public String getAttributIDName() {
+        return this.getId();
+    }
+
+    @Override
+    public String getTuppleID() {
+        return "id_type_rindrina";
+    }
+}
