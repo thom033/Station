89bfdb0c2e9c7610f453c5680bbe2ebf7921cb41
RASOLOMANDIMBY Nomenjanahary Thomis <Thomis.rasolomandimby@gmail.com>
2025-01-22 10:53:35 +0300

    Tohiny Servlet

--- station-war/src/java/servlet/CarteServlet.java
+++ station-war/src/java/servlet/CarteServlet.java
@@ -9,10 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 import bean.CGenUtil;
 
-import hetra.trano.TypeRindrina;
-import hetra.trano.TypeTafo;
+import hetra.trano.*;
 
 @WebServlet( urlPatterns = {"/carte"})
 public class CarteServlet  extends HttpServlet{
@@ -20,13 +21,23 @@ public class CarteServlet  extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
+            Gson gson - new Gson();
             TypeRindrina typeRindrina = new TypeRindrina();
-            TypeTafo typeTafo = new TypeTafo();
+            TypeTafo typeTafo = new TypeTafo()
 
             TypeRindrina[] typeRindrinaList = (TypeRindrina[]) CGenUtil.rechercher(typeRindrina, null, null, null, "");
             TypeTafo[] typeTafoList = (TypeTafo[]) CGenUtil.rechercher(typeTafo, null, null, null, "");
-            req.setAttribute("typeRindrinaList", typeRindrinaList);
-            req.setAttribute("typeTafoList", typeTafoList);
+            
+            TypeData data = new TypeData(typeRindrinaList,typeTafoList);
+
+            resp.setHeader("Access-Control-Allow-Origin", "*");
+            resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+            String jsonResponse = gson.toJson(data);
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.getWriter().write(jsonResponse);
             
         } catch (Exception e) {
             e.printStackTrace();
