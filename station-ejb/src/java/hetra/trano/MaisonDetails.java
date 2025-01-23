5d54e158d51fa76965961552658f1767561ea8f6
RASOLOMANDIMBY Nomenjanahary Thomis <Thomis.rasolomandimby@gmail.com>
2025-01-23 19:38:47 +0300

    Update MaisonDetails.java

--- station-ejb/src/java/hetra/trano/MaisonDetails.java
+++ station-ejb/src/java/hetra/trano/MaisonDetails.java
@@ -5,8 +5,27 @@ import java.sql.Connection;
 import bean.ClassMAPTable;
 
 public class MaisonDetails extends ClassMAPTable {
-    String id_maison_detaills, id_maison;
-    String id_type_tafo, id_type_rindrina;
+    String id_maison_details, id_maison, id_type_tafo, id_type_rindrina;
+    int nb_etages;
+    double longueur,largeur;
+
+    //getters and setters
+    public String getId_maison_details() {
+        return id_maison_details;
+    }
+
+    public void setId_maison_details(String id_maison_details) {
+        this.id_maison_details = id_maison_details;
+    }
+
+    public String getId_maison() {
+        return id_maison;
+    }
+
+    public void setId_maison(String id_maison) {
+        this.id_maison = id_maison;
+    }
+
     public String getId_type_tafo() {
         return id_type_tafo;
     }
@@ -23,22 +42,30 @@ public class MaisonDetails extends ClassMAPTable {
         this.id_type_rindrina = id_type_rindrina;
     }
 
-    public String getId_maison_detaills() {
-        return id_maison_detaills;
+    public int getNb_etages() {
+        return nb_etages;
     }
 
-    public void setId_maison_detaills(String id_maison_detaills) {
-        this.id_maison_detaills = id_maison_detaills;
+    public void setNb_etages(int nb_etages) {
+        this.nb_etages = nb_etages;
     }
 
-    public String getId_maison() {
-        return id_maison;
+    public double getLongueur() {
+        return longueur;
     }
 
-    public void setId_maison(String id_maison) {
-        this.id_maison = id_maison;
+    public void setLongueur(double longueur) {
+        this.longueur = longueur;
     }
 
+    public double getLargeur() {
+        return largeur;
+    }
+
+    public void setLargeur(double largeur) {
+        this.largeur = largeur;
+    }
+    
     public MaisonDetails() {
         this.setNomTable("MAISON_DETAILLS");
     }
@@ -52,7 +79,7 @@ public class MaisonDetails extends ClassMAPTable {
     @Override
     public void construirePK(Connection c) throws Exception {
         this.preparePk("MD", "get_seq_maison_detaills");
-        this.setId_maison_detaills(makePK(c));
+        this.setId_maison_details(makePK(c));
     }
 
     @Override
@@ -62,7 +89,7 @@ public class MaisonDetails extends ClassMAPTable {
 
     @Override
     public String getTuppleID() {
-        return this.id_maison_detaills;
+        return this.id_maison_details;
     }
     
 }
