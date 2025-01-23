d88ee9b3d48af8ba2c091cc0bdc204a3ed23fa91
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-23 15:56:09 +0300

    Update Maison.java

--- station-ejb/src/java/hetra/trano/Maison.java
+++ station-ejb/src/java/hetra/trano/Maison.java
@@ -1,29 +1,21 @@
 package hetra.trano;
 
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
-
-import oracle.sql.STRUCT;
-import oracle.sql.ARRAY;
-
-import bean.ClassMAPTable;
 import bean.ClassMere;
 import utilitaire.UtilDB;
 
 
 public class Maison extends ClassMere {
     
-    String id, nom;
-    double longueur, largeur;
-    int etage;
-    MaisonDetails details;
+    String id_maison,id_proprietaire, nom;
     double latitude, longitude;
+
+    MaisonDetails details;
 
     public void setDetails(MaisonDetails details) {
         this.details = details;
@@ -31,34 +23,21 @@ public class Maison extends ClassMere {
     public MaisonDetails getDetails() {
         return details;
     }
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-     // Méthode pour vérifier si la longitude est définie (non NaN)
-     public boolean isLongitudeDefined() {
-        return !Double.isNaN(longitude);
+
+    public String getId_maison() {
+        return id_maison;
     }
 
-    // Méthode pour vérifier si la latitude est définie (non NaN)
-    public boolean isLatitudeDefined() {
-        return !Double.isNaN(latitude);
-    }
-    public double getLatitude() {
-        return latitude;
-    }
-    public double getLongitude() {
-        return longitude;
+    public void setId_maison(String id_maison) {
+        this.id_maison = id_maison;
     }
 
-    public String getId() {
-        return id;
+    public String getId_proprietaire() {
+        return id_proprietaire;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setId_proprietaire(String id_proprietaire) {
+        this.id_proprietaire = id_proprietaire;
     }
 
     public String getNom() {
@@ -69,78 +48,50 @@ public class Maison extends ClassMere {
         this.nom = nom;
     }
 
-    public double getLongueur() {
-        return longueur;
+    public double getLatitude() {
+        return latitude;
     }
 
-    public void setLongueur(double longueur) {
-        this.longueur = longueur;
-    }
-    
-    public double getLargeur() {
-        return largeur;
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 
-    public void setLargeur(double largeur) {
-        this.largeur = largeur;
+    public double getLongitude() {
+        return longitude;
     }
 
-    public int getEtage() {
-        return etage;
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
-
-    public void setEtage(int etage) {
-        this.etage = etage;
-    }
-
-
-    public Maison () {
-        this.setNomTable("Maison");
-    }
-
-    public Maison(String nom2, double longueur2, double largeur2, int nbrEtage, double latitude2, double longitude2) {
-        setNom(nom2);setLongueur(longueur2);setLargeur(largeur2);setEtage(nbrEtage);setLatitude(latitude2);setLongitude(longitude2);
-    }
-
-    @Override
-    public void construirePK(Connection c) throws Exception {
-        this.preparePk("TR", "get_seq_maison");
-        this.setId(makePK(c));
-    }
-    
-
     @Override
     public String getAttributIDName() {
-        return this.getId();
+        return this.getId_maison();
     }
 
     @Override
     public String getTuppleID() {
-        return "id";
+        return "id_maison";
     }
 
 
     @Override
     public int insertToTable() throws Exception {
-        String sql = "INSERT INTO maison (id, nom, longeur, largeur, nbr_etage, position) " +
+        String sql = "INSERT INTO maison (id_maison, nom,  position) " +
         "VALUES (?, ?, ?, ?, ?, SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(?, ?, NULL), NULL, NULL))";
         try (Connection c = new UtilDB().GetConn();
             PreparedStatement statement = c.prepareStatement(sql)) {
                 this.construirePK(c);
 
             // Paramètres de la requête
-            statement.setString(1, this.getId());
+            statement.setString(1, this.getId_maison());
             statement.setString(2, this.getNom());
-            statement.setDouble(3, this.getLongueur());
-            statement.setDouble(4, this.getLargeur());
-            statement.setInt(5, this.getEtage());
-            statement.setDouble(6, this.getLatitude()); // Latitude
-            statement.setDouble(7, this.getLongitude()); // Longitude
+            statement.setDouble(3, this.getLatitude()); // Latitude
+            statement.setDouble(4, this.getLongitude()); // Longitude
             // Exécution de la requête
             int rowsInserted = statement.executeUpdate();
 
-            this.getDetails().setId_maison(this.getId());
-            this.getDetails().insertToTable(c);
+            // this.getDetails().setId_maison(this.getId());
+            // this.getDetails().insertToTable(c);
 
             if (rowsInserted > 0) {
                 System.out.println("Insertion réussie dans la table `maison`.");
@@ -153,18 +104,15 @@ public class Maison extends ClassMere {
 
     public static List<Maison> getAllMaison(Connection connection) throws Exception {
         List<Maison> maisons = new ArrayList<>();
-        String sql = "SELECT id, nom, longeur, largeur, nbr_etage, latitude, longitude FROM maison";
+        String sql = "SELECT id, nom, latitude, longitude FROM maison";
     
         try (PreparedStatement statement = connection.prepareStatement(sql);
              ResultSet resultSet = statement.executeQuery()) {
     
             while (resultSet.next()) {
                 Maison maison = new Maison();
-                maison.setId(resultSet.getString("id"));
+                maison.setId_maison(resultSet.getString("id"));
                 maison.setNom(resultSet.getString("nom"));
-                maison.setLongueur(resultSet.getDouble("longeur"));
-                maison.setLargeur(resultSet.getDouble("largeur"));
-                maison.setEtage(resultSet.getInt("nbr_etage"));
                 maison.setLatitude(resultSet.getDouble("latitude"));  // Récupère la latitude
                 maison.setLongitude(resultSet.getDouble("longitude")); // Récupère la longitude
     
