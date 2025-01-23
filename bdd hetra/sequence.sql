3c341e56e9cd737e9ad8c23e0034335f0ac7d278
RASOLOMANDIMBY Nomenjanahary Thomis <Thomis.rasolomandimby@gmail.com>
2025-01-23 21:00:37 +0300

    Update sequence.sql

--- bdd hetra/sequence.sql
+++ bdd hetra/sequence.sql
@@ -1,26 +1,5 @@
--- Sequence for type_tafo
-CREATE SEQUENCE seq_type_tafo
-START WITH 1
-INCREMENT BY 1
-NOCACHE
-NOCYCLE;
-
--- Sequence for type_rindrina
-CREATE SEQUENCE seq_type_rindrina
-START WITH 1
-INCREMENT BY 1
-NOCACHE
-NOCYCLE;
-
--- Sequence for maison
-CREATE SEQUENCE seq_maison
-START WITH 1
-INCREMENT BY 1
-NOCACHE
-NOCYCLE;
-
--- Sequence for maison_detaills
-CREATE SEQUENCE seq_maison_detaills
+-- Sequence for commune
+CREATE SEQUENCE seq_commune
 START WITH 1
 INCREMENT BY 1
 NOCACHE
@@ -33,79 +12,65 @@ INCREMENT BY 1
 NOCACHE
 NOCYCLE;
 
--- Sequence for arrondissement_position
-CREATE SEQUENCE seq_arrondissement_position
+-- Sequence for polygone
+CREATE SEQUENCE seq_polygone
 START WITH 1
 INCREMENT BY 1
 NOCACHE
 NOCYCLE;
 
--- Sequence for hetra
-CREATE SEQUENCE seq_hetra
+-- Sequence for prix_impot
+CREATE SEQUENCE seq_prix_impot
 START WITH 1
 INCREMENT BY 1
 NOCACHE
 NOCYCLE;
 
--- Sequence for paiementP
-CREATE SEQUENCE seq_paiementP
+-- Sequence for maison
+CREATE SEQUENCE seq_maison
 START WITH 1
 INCREMENT BY 1
 NOCACHE
 NOCYCLE;
 
--- Function for seq_type_tafo
-CREATE OR REPLACE FUNCTION get_seq_type_tafo RETURN NUMBER IS
-BEGIN
-    RETURN seq_type_tafo.NEXTVAL;
-END;
-/
+-- Sequence for maison_details
+CREATE SEQUENCE seq_maison_details
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
 
--- Function for seq_type_rindrina
-CREATE OR REPLACE FUNCTION get_seq_type_rindrina RETURN NUMBER IS
-BEGIN
-    RETURN seq_type_rindrina.NEXTVAL;
-END;
-/
+-- Sequence for type_tafo
+CREATE SEQUENCE seq_type_tafo
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
 
--- Function for seq_maison
-CREATE OR REPLACE FUNCTION get_seq_maison RETURN NUMBER IS
-BEGIN
-    RETURN seq_maison.NEXTVAL;
-END;
-/
+-- Sequence for type_tafo_coefficient
+CREATE SEQUENCE seq_type_tafo_coefficient
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
 
--- Function for seq_maison_detaills
-CREATE OR REPLACE FUNCTION get_seq_maison_detaills RETURN NUMBER IS
-BEGIN
-    RETURN seq_maison_detaills.NEXTVAL;
-END;
-/
+-- Sequence for type_rindrina
+CREATE SEQUENCE seq_type_rindrina
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
 
--- Function for seq_arrondissement
-CREATE OR REPLACE FUNCTION get_seq_arrondissement RETURN NUMBER IS
-BEGIN
-    RETURN seq_arrondissement.NEXTVAL;
-END;
-/
+-- Sequence for type_rindrina_coefficient
+CREATE SEQUENCE seq_type_rindrina_coefficient
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
 
--- Function for seq_arrondissement_position
-CREATE OR REPLACE FUNCTION get_seq_arrondissement_p RETURN NUMBER IS
-BEGIN
-    RETURN seq_arrondissement_position.NEXTVAL;
-END;
-/
-
--- Function for seq_hetra
-CREATE OR REPLACE FUNCTION get_seq_hetra RETURN NUMBER IS
-BEGIN
-    RETURN seq_hetra.NEXTVAL;
-END;
-/
-
--- Function for seq_paiementP
-CREATE OR REPLACE FUNCTION get_seq_paiementP RETURN NUMBER IS
-BEGIN
-    RETURN seq_paiementP.NEXTVAL;
-END;
-/
+-- Sequence for proprietaire
+CREATE SEQUENCE seq_proprietaire
+START WITH 1
+INCREMENT BY 1
+NOCACHE
+NOCYCLE;
