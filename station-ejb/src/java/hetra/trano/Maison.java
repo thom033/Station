f6f600f47974060d4274eab0d7a4877064a03e73
RASOLOMANDIMBY Nomenjanahary Thomis <Thomis.rasolomandimby@gmail.com>
2025-01-23 19:38:44 +0300

    Update Maison.java

--- station-ejb/src/java/hetra/trano/Maison.java
+++ station-ejb/src/java/hetra/trano/Maison.java
@@ -9,37 +9,25 @@ import utilitaire.UtilDB;
 
 public class Maison extends ClassMere {
     
-    String id, nom;
-    double longueur, largeur;
-    int etage;
+    String id_maison,id_proprietaire, nom;
     MaisonDetails details;
     double latitude, longitude;
 
-    public void setDetails(MaisonDetails details) {
-        this.details = details;
-    }
-    public MaisonDetails getDetails() {
-        return details;
-    }
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-    public double getLatitude() {
-        return latitude;
-    }
-    public double getLongitude() {
-        return longitude;
+    // Getters and Setters
+    public String getId_maison() {
+        return id_maison;
     }
 
-    public String getId() {
-        return id;
+    public void setId_maison(String id_maison) {
+        this.id_maison = id_maison;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getId_proprietaire() {
+        return id_proprietaire;
+    }
+
+    public void setId_proprietaire(String id_proprietaire) {
+        this.id_proprietaire = id_proprietaire;
     }
 
     public String getNom() {
@@ -50,49 +38,44 @@ public class Maison extends ClassMere {
         this.nom = nom;
     }
 
-    public double getLongueur() {
-        return longueur;
+    public MaisonDetails getDetails() {
+        return details;
     }
 
-    public void setLongueur(double longueur) {
-        this.longueur = longueur;
-    }
-    
-    public double getLargeur() {
-        return largeur;
+    public void setDetails(MaisonDetails details) {
+        this.details = details;
     }
 
-    public void setLargeur(double largeur) {
-        this.largeur = largeur;
+    public double getLatitude() {
+        return latitude;
     }
 
-    public int getEtage() {
-        return etage;
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 
-    public void setEtage(int etage) {
-        this.etage = etage;
+    public double getLongitude() {
+        return longitude;
     }
 
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 
     public Maison () {
         this.setNomTable("Maison");
     }
 
-    public Maison(String nom2, double longueur2, double largeur2, int nbrEtage, double latitude2, double longitude2) {
-        setNom(nom2);setLongueur(longueur2);setLargeur(largeur2);setEtage(nbrEtage);setLatitude(latitude2);setLongitude(longitude2);
-    }
-
     @Override
     public void construirePK(Connection c) throws Exception {
         this.preparePk("TR", "get_seq_maison");
-        this.setId(makePK(c));
+        this.setId_maison(makePK(c));
     }
     
 
     @Override
     public String getAttributIDName() {
-        return this.getId();
+        return this.getId_maison();
     }
 
     @Override
@@ -103,34 +86,30 @@ public class Maison extends ClassMere {
 
     @Override
     public int insertToTable() throws Exception {
-        String sql = "INSERT INTO maison (id, nom, longeur, largeur, nbr_etage, position) " +
-        "VALUES (?, ?, ?, ?, ?, SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(?, ?, NULL), NULL, NULL))";
+        String sql = "INSERT INTO maison (id_maison, id_proprietaire, nom, position) " +
+        "VALUES (?, ?, ?, SDO_GEOMETRY(2001, 4326, SDO_POINT_TYPE(?, ?, NULL), NULL, NULL))";
         try (Connection c = new UtilDB().GetConn();
             PreparedStatement statement = c.prepareStatement(sql)) {
                 this.construirePK(c);
 
             // Paramètres de la requête
-            statement.setString(1, this.getId());
-            statement.setString(2, this.getNom());
-            statement.setDouble(3, this.getLongueur());
-            statement.setDouble(4, this.getLargeur());
-            statement.setInt(5, this.getEtage());
-            statement.setDouble(6, this.getLongitude()); // Longitude
-            statement.setDouble(7, this.getLatitude()); // Latitude
+            statement.setString(1, this.getId_maison());
+            statement.setString(2, this.getId_proprietaire());
+            statement.setString(3, this.getNom());
+            statement.setDouble(4, this.getLongitude()); // Longitude
+            statement.setDouble(5, this.getLatitude()); // Latitude
+
             // Exécution de la requête
             int rowsInserted = statement.executeUpdate();
 
-            this.getDetails().setId_maison(this.getId());
+            this.getDetails().setId_maison(this.getId_maison());
             this.getDetails().insertToTable(c);
 
             if (rowsInserted > 0) {
                 System.out.println("Insertion réussie dans la table `maison`.");
             }
+
             return rowsInserted;
-        } catch (Exception e) {
-            throw e;
         }
     }
-
-    
 }
