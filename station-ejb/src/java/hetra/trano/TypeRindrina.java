bc5160608edf4651e20fe289a1ce85b3b50f9c2a
Mahaliana <153856982+mahalianafitiavana@users.noreply.github.com>
2025-01-24 17:24:27 +0300

    Update TypeRindrina.java

--- station-ejb/src/java/hetra/trano/TypeRindrina.java
+++ station-ejb/src/java/hetra/trano/TypeRindrina.java
@@ -8,7 +8,7 @@ import bean.ClassMAPTable;
 
 public class TypeRindrina extends ClassMAPTable{
     String id_type_rindrina,nom;
-    int coefficient;
+    double coefficient;
     
     public String getId_type_rindrina() {
         return id_type_rindrina;
@@ -26,11 +26,11 @@ public class TypeRindrina extends ClassMAPTable{
         this.nom = nom;
     }
 
-    public int getCoefficient() {
+    public double getCoefficient() {
         return coefficient;
     }
 
-    public void setCoefficient(int coefficient) {
+    public void setCoefficient(double coefficient) {
         this.coefficient = coefficient;
     }
 
